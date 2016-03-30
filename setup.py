ec7f6135d9d44844985b26226a61cf3a045490ef
Adam Tomkins <artomkins@googlemail.com>
2016-03-30 15:09:41 +0100

    add setuptools to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
+import setuptools
  
 setup(name = "libSpineML",
       version = "0.1",
       description = "Python bindings for the SpineML specifications",
       author = "Adam Tomkins",
       author_email = "a.tomkins@sheffield.ac.uk",
-      packages=["libSpineML"]
+      packages=["libSpineML"],
+      install_requires=[
+          'lxml >= 3.3.0'
+      ]
       )
