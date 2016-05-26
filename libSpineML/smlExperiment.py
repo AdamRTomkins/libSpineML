83ce514bf2dfc84892e0bacd9e786b38ce80a3a3
Adam Tomkins <artomkins@googlemail.com>
2016-05-26 16:44:31 +0100

    Update Experiment formating and removing SpineMLType name

--- libSpineML/smlExperiment.py
+++ libSpineML/smlExperiment.py
@@ -668,7 +668,7 @@ class SpineMLType(GeneratedsSuper):
             return True
         else:
             return False
-    def export(self, outfile, level, namespace_='', name_='SpineMLType', namespacedef_='', pretty_print=True):
+    def export(self, outfile, level, namespace_='', name_='SpineML', namespacedef_='', pretty_print=True):
         if pretty_print:
             eol_ = '\n'
         else:
@@ -678,17 +678,17 @@ class SpineMLType(GeneratedsSuper):
         showIndent(outfile, level, pretty_print)
         outfile.write('<%s%s%s' % (namespace_, name_, namespacedef_ and ' ' + namespacedef_ or '', ))
         already_processed = set()
-        self.exportAttributes(outfile, level, already_processed, namespace_, name_='SpineMLType')
+        self.exportAttributes(outfile, level, already_processed, namespace_, name_='SpineML')
         if self.hasContent_():
             outfile.write('>%s' % (eol_, ))
-            self.exportChildren(outfile, level + 1, namespace_='', name_='SpineMLType', pretty_print=pretty_print)
+            self.exportChildren(outfile, level + 1, namespace_='', name_='SpineML', pretty_print=pretty_print)
             showIndent(outfile, level, pretty_print)
             outfile.write('</%s%s>%s' % (namespace_, name_, eol_))
         else:
             outfile.write('/>%s' % (eol_, ))
-    def exportAttributes(self, outfile, level, already_processed, namespace_='', name_='SpineMLType'):
+    def exportAttributes(self, outfile, level, already_processed, namespace_='', name_='SpineML'):
         pass
-    def exportChildren(self, outfile, level, namespace_='', name_='SpineMLType', fromsubclass_=False, pretty_print=True):
+    def exportChildren(self, outfile, level, namespace_='', name_='SpineML', fromsubclass_=False, pretty_print=True):
         if pretty_print:
             eol_ = '\n'
         else:
